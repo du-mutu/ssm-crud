1dbffca2a178fc874c3b3314c0da7b70eec74f10
mudaotusheng <8672652+mudaotusheng@user.noreply.gitee.com>
2021-08-25 13:56:03 +0800

    hot-fix first commit

--- src/main/java/com/atguigu/crud/test/MvcTest.java
+++ src/main/java/com/atguigu/crud/test/MvcTest.java
@@ -57,6 +57,7 @@ public class MvcTest {
             System.out.println("总页码："+pi.getPages());
             System.out.println("总记录数："+pi.getTotal());
             System.out.println("在页面需要连续显示的页码");
+            System.out.println("master添加");
             int[] nums = pi.getNavigatepageNums();
             for (int i : nums) {
                 System.out.print(" "+i);
